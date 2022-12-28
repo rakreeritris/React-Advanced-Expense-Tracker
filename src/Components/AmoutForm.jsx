2c4c72094135240acc277eb7e20717252a39f66e
Ritik Gupta <rakreeritris@gmail.com>
2022-12-28 20:27:30 +0530

    fixed the navbar of home page

--- src/Components/AmoutForm.jsx
+++ src/Components/AmoutForm.jsx
@@ -1,57 +1,70 @@
-import React, { useContext, useState } from 'react'
-import { doc, updateDoc,arrayUnion,getDoc } from "firebase/firestore";
-import {db} from '../firebase';
-import { AuthContext } from '../context/AuthContext';
+import React, { useContext, useState } from "react";
+import { doc, updateDoc, arrayUnion, getDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 import { v4 as uuid } from "uuid";
 function AmoutForm() {
-const [msg,setMsg]=useState("");
-const {currentUser}=useContext(AuthContext);
+  const [msg, setMsg] = useState("");
+  const { currentUser } = useContext(AuthContext);
 
-const handleSubmit=async(e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const amount=parseInt(e.target[0].value);
-    const person=e.target[1].value;
-    const typeofAmount=e.target[2].value;
-    const Msg=msg;
-  /*   console.log(amount,person,typeofAmount,Msg);
+    const amount = parseInt(e.target[0].value);
+    const person = e.target[1].value;
+    const typeofAmount = e.target[2].value;
+    const Msg = msg;
+    /*   console.log(amount,person,typeofAmount,Msg);
     console.log(typeof(amount)); */
-    const user = doc(db, "users",currentUser.uid);
+    const user = doc(db, "users", currentUser.uid);
     const docSnap = await getDoc(user);
 
-  const prevCreditAmount=parseInt(docSnap.data().creditAmount);
-  const prevDebitAmount=parseInt(docSnap.data().debitAmount);
-/*   console.log(prevDebitAmount,typeof(prevDebitAmount));
+    const prevCreditAmount = parseInt(docSnap.data().creditAmount);
+    const prevDebitAmount = parseInt(docSnap.data().debitAmount);
+    /*   console.log(prevDebitAmount,typeof(prevDebitAmount));
   console.log(prevCreditAmount,typeof(prevCreditAmount)); */
-await updateDoc(user, {
-  creditAmount:typeofAmount==='CREDIT' ? amount+prevCreditAmount:prevCreditAmount,
-  debitAmount:typeofAmount==='DEBIT' ? amount+prevDebitAmount:prevDebitAmount,
-  TransactionArray:arrayUnion({
-     amount,
-     person,
-     typeofAmount,
-     Msg,
-     Id:uuid()  
-  })
-
-});
-    
-
-}
+    await updateDoc(user, {
+      creditAmount:
+        typeofAmount === "CREDIT"
+          ? amount + prevCreditAmount
+          : prevCreditAmount,
+      debitAmount:
+        typeofAmount === "DEBIT" ? amount + prevDebitAmount : prevDebitAmount,
+      TransactionArray: arrayUnion({
+        amount,
+        person,
+        typeofAmount,
+        Msg,
+        Id: uuid(),
+      }),
+    });
+  };
   return (
     <div>
-      <form onSubmit={ handleSubmit}>
-        <label ></label>
-       <input type='number' placeholder="Enter the amount..."></input>
-       <input type='text' placeholder='Name of the person...'></input>
-   <select name="Type" >
-  <option value="CREDIT">CREDIT</option>
-  <option value="DEBIT">DEBIT</option>
-</select>
-  <textarea onChange={(e)=>setMsg(e.target.value)} placeholder='Enter a short message...'></textarea>
-  <button type='submit'>Submit</button>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input type="number" placeholder="Enter the amount..."></input>
+        </div>
+        <div>
+          <input type="text" placeholder="Name of the person..."></input>
+        </div>
+        <div>
+          <select name="Type">
+            <option value="CREDIT">CREDIT</option>
+            <option value="DEBIT">DEBIT</option>
+          </select>
+        </div>
+        <div>
+          <textarea
+            onChange={(e) => setMsg(e.target.value)}
+            placeholder="Enter a short message..."
+          ></textarea>
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
       </form>
     </div>
-  )
+  );
 }
 
-export default AmoutForm
+export default AmoutForm;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,25 +1,64 @@
-import React,{useContext} from 'react'
-import AmoutForm from '../Components/AmoutForm'
-import Credit from '../Components/Credit'
-import Debit from '../Components/Debit'
-import {signOut} from 'firebase/auth';
-import { auth } from '../firebase';
-import { AuthContext } from '../context/AuthContext';
-import './Home.css'
-import PieChart from '../Components/PieChart';
+import React, { useContext } from "react";
+import AmoutForm from "../Components/AmoutForm";
+import Credit from "../Components/Credit";
+import Debit from "../Components/Debit";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
+import img from "../Images/logo.jpg";
+import "./Home.css";
+import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
+import logout from "../Images/logout.svg";
+import LogoutSharpIcon from "@mui/icons-material/LogoutSharp";
+/* import PieChart from "../Components/PieChart"; */
 function Home() {
-    const {currentUser}=useContext(AuthContext);
+  const { currentUser } = useContext(AuthContext);
   return (
-    <div>
-        <h1>{currentUser.displayName}</h1>
-        <img src={currentUser.photoURL} alt='user profile' id='img'></img>
-    <AmoutForm></AmoutForm>
-      <Credit></Credit>
-      <Debit></Debit>
-      <PieChart></PieChart>
-    <button onClick={()=>signOut(auth)}>Logout</button>
-    </div>
-  )
+    <>
+      <div className="home-nav">
+        <div className="nav-heading">
+          <img src={img} alt="logo" className="nav-logoimg" />
+          <span className="nav-logo-title">EXPENSE TRACKER</span>
+        </div>
+        <div className="nav-profile">
+          <img
+            src={currentUser.photoURL}
+            alt="user profile"
+            className="nav-profileimg"
+          ></img>
+          <span className="nav-profilename">{currentUser.displayName}</span>
+          <button onClick={() => signOut(auth)} className="nav-logout">
+            {/*     <img src={logout} alt="logout"></img> */}
+            <LogoutSharpIcon></LogoutSharpIcon>
+            LOGOUT
+          </button>
+        </div>
+      </div>
+      <div className="creditsanddebits">
+        <div className="credit">
+          <span>CREDIT</span>
+          <div className="credit-amt">
+            <span>80</span>
+            <CurrencyRupeeIcon></CurrencyRupeeIcon>
+          </div>
+        </div>
+        <div className="debit">
+          <span>DEBIT</span>
+          <div className="debit-amt">
+            <span>80</span>
+            <CurrencyRupeeIcon></CurrencyRupeeIcon>
+          </div>
+        </div>
+      </div>
+      <div>
+        <AmoutForm></AmoutForm>
+      </div>
+
+      {/*   <div>
+        <PieChart></PieChart>
+      </div> */}
+    </>
+  );
 }
 
-export default Home
+export default Home;
